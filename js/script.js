cf84cd5c9166d3545be0ee9a820c755e43194e8d
francis-mbithi <frankmbithifm@gmail.com>
2019-09-30 09:17:21 +0300

    add hover to class wedo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ 
+ $(".first").click(function() {
+    $(".wedo1").toggle(500);
+    $(".what1").toggle(500);
+  });
+  $(".second").click(function() {
+    $(".wedo2").toggle(500);
+    $(".what2").toggle(500);
+  });
+  $(".third").click(function() {
+    $(".wedo3").toggle(500);
+    $(".what3").toggle(500);
+  });
+ 
+        
+        
+            
+    
